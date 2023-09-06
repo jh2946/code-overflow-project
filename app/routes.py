75aadcca4ae892891ece9a50ebfd95b49905db33
Your Name <you@example.com>
2023-09-07 04:12:27 +0800

    working

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,9 @@
 import secrets
 import os
+import math
+import numpy as np
+from werkzeug.utils import secure_filename
+from tensorflow.keras.preprocessing import image
 from flask import render_template, url_for, flash, redirect, request, send_file
 from app import app, db, bcrypt
 from werkzeug.utils import secure_filename
@@ -26,6 +30,217 @@ def register():
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
+start = [0]
+passed = [0]
+pack = [[]]
+num = [0]
+@app.route('/upload', methods=['POST'])
+def upload():
+    file = request.files.getlist("img")
+    for f in file:
+        filename = secure_filename(str(num[0] + 500) + '.jpg')
+        num[0] += 1
+        name = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        print('save name', name)
+        f.save(name)
+
+    pack[0] = []
+    return render_template('submit.html', img=file, name=name)
+
+import tensorflow
+import csv
+from tensorflow.python.keras.models import load_model
+# define label meaning
+label = ['apple pie',
+         'baby back ribs',
+         'baklava',
+         'beef carpaccio',
+         'beef tartare',
+         'beet salad',
+         'beignets',
+         'bibimbap',
+         'bread pudding',
+         'breakfast burrito',
+         'bruschetta',
+         'caesar salad',
+         'cannoli',
+         'caprese salad',
+         'carrot cake',
+         'ceviche',
+         'cheese plate',
+         'cheesecake',
+         'chicken curry',
+         'chicken quesadilla',
+         'chicken wings',
+         'chocolate cake',
+         'chocolate mousse',
+         'churros',
+         'clam chowder',
+         'club sandwich',
+         'crab cakes',
+         'creme brulee',
+         'croque madame',
+         'cup cakes',
+         'deviled eggs',
+         'donuts',
+         'dumplings',
+         'edamame',
+         'eggs benedict',
+         'escargots',
+         'falafel',
+         'filet mignon',
+         'fish and_chips',
+         'foie gras',
+         'french fries',
+         'french onion soup',
+         'french toast',
+         'fried calamari',
+         'fried rice',
+         'frozen yogurt',
+         'garlic bread',
+         'gnocchi',
+         'greek salad',
+         'grilled cheese sandwich',
+         'grilled salmon',
+         'guacamole',
+         'gyoza',
+         'hamburger',
+         'hot and sour soup',
+         'hot dog',
+         'huevos rancheros',
+         'hummus',
+         'ice cream',
+         'lasagna',
+         'lobster bisque',
+         'lobster roll sandwich',
+         'macaroni and cheese',
+         'macarons',
+         'miso soup',
+         'mussels',
+         'nachos',
+         'omelette',
+         'onion rings',
+         'oysters',
+         'pad thai',
+         'paella',
+         'pancakes',
+         'panna cotta',
+         'peking duck',
+         'pho',
+         'pizza',
+         'pork chop',
+         'poutine',
+         'prime rib',
+         'pulled pork sandwich',
+         'ramen',
+         'ravioli',
+         'red velvet cake',
+         'risotto',
+         'samosa',
+         'sashimi',
+         'scallops',
+         'seaweed salad',
+         'shrimp and grits',
+         'spaghetti bolognese',
+         'spaghetti carbonara',
+         'spring rolls',
+         'steak',
+         'strawberry shortcake',
+         'sushi',
+         'tacos',
+         'octopus balls',
+         'tiramisu',
+         'tuna tartare',
+         'waffles'] # sorry
+nu_link = 'https://www.nutritionix.com/food/'
+nutrients = [
+    {'name': 'protein', 'value': 0.0},
+    {'name': 'calcium', 'value': 0.0},
+    {'name': 'fat', 'value': 0.0},
+    {'name': 'carbohydrates', 'value': 0.0},
+    {'name': 'vitamins', 'value': 0.0}
+]
+
+with open('nutrition101.csv', 'r') as file:
+    reader = csv.reader(file)
+    nutrition_table = dict()
+    for i, row in enumerate(reader):
+        if i == 0:
+            name = ''
+            continue
+        else:
+            name = row[1].strip()
+        nutrition_table[name] = [
+            {'name': 'protein', 'value': float(row[2])},
+            {'name': 'calcium', 'value': float(row[3])},
+            {'name': 'fat', 'value': float(row[4])},
+            {'name': 'carbohydrates', 'value': float(row[5])},
+            {'name': 'vitamins', 'value': float(row[6])}
+        ]
+@app.route('/predict')
+def predict():
+    # Loading the best saved model to make predictions.
+    tensorflow.keras.backend.clear_session()
+    model_best = load_model('best_model_101class.h5', compile=False)
+    # model_best = load_model('model_trained_101class.h5', compile=False)
+    print('model successfully loaded!')
+    result = []
+    # pack = []
+    print('total image', num[0])
+    for i in range(start[0], num[0]):
+        pa = dict()
+
+        filename = f'{app.config["UPLOAD_FOLDER"]}/{i + 500}.jpg'
+        print('image filepath', filename)
+        pred_img = filename
+        pred_img = image.load_img(pred_img, target_size=(200, 200))
+        pred_img = image.img_to_array(pred_img)
+        pred_img = np.expand_dims(pred_img, axis=0)
+        pred_img = pred_img / 255.
+
+        pred = model_best.predict(pred_img)
+        print("Pred")
+        print(pred)
+
+        if math.isnan(pred[0][0]) and math.isnan(pred[0][1]) and \
+                math.isnan(pred[0][2]) and math.isnan(pred[0][3]):
+            pred = np.array([0.05, 0.05, 0.05, 0.07, 0.09, 0.19, 0.55, 0.0, 0.0, 0.0, 0.0])
+
+        top = pred.argsort()[0][-3:]
+        label.sort()
+        _true = label[top[2]]
+        pa['image'] = f'{app.config["UPLOAD_FOLDER"]}/{i + 500}.jpg'
+        x = dict()
+        x[_true] = float("{:.2f}".format(pred[0][top[2]] * 100))
+        x[label[top[1]]] = float("{:.2f}".format(pred[0][top[1]] * 100))
+        x[label[top[0]]] = float("{:.2f}".format(pred[0][top[0]] * 100))
+        pa['result'] = x
+        pa['nutrition'] = nutrition_table[_true]
+        pa['food'] = f'{nu_link}{_true}'
+        pa['idx'] = i - start[0]
+        pa['quantity'] = 100
+
+        pack[0].append(pa)
+        passed[0] += 1
+
+    start[0] = passed[0]
+    print('successfully packed')
+    # compute the average source of calories
+    for p in pack[0]:
+        nutrients[0]['value'] = (nutrients[0]['value'] + p['nutrition'][0]['value'])
+        nutrients[1]['value'] = (nutrients[1]['value'] + p['nutrition'][1]['value'])
+        nutrients[2]['value'] = (nutrients[2]['value'] + p['nutrition'][2]['value'])
+        nutrients[3]['value'] = (nutrients[3]['value'] + p['nutrition'][3]['value'])
+        nutrients[4]['value'] = (nutrients[4]['value'] + p['nutrition'][4]['value'])
+
+    nutrients[0]['value'] = nutrients[0]['value'] / num[0]
+    nutrients[1]['value'] = nutrients[1]['value'] / num[0]
+    nutrients[2]['value'] = nutrients[2]['value'] / num[0]
+    nutrients[3]['value'] = nutrients[3]['value'] / num[0]
+    nutrients[4]['value'] = nutrients[4]['value'] / num[0]
+
+    return render_template('results.html', pack=pack[0], whole_nutrition=nutrients)
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
@@ -41,20 +256,20 @@ def login():
             flash('Login Unsuccessful. Please check email and password', 'danger')
     return render_template('login.html', title='Login', form=form)
 
-@app.route('/uploads')
-def uploads():
-    return send_file(os.path.join(app.config['UPLOAD_FOLDER'], request.args['filename']))
+# @app.route('/uploads')
+# def uploads():
+#     return send_file(os.path.join(app.config['UPLOAD_FOLDER'], request.args['filename']))
 
 @app.route('/submit', methods=['GET', 'POST'])
 def submit():
     form = UploadForm()
-    if form.validate_on_submit():
-        file = request.files['photo']
-        filename = secrets.token_urlsafe(16)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        print(url_for('uploads', filename=filename))
-    else:
-        print('error', form.errors)
+    # if form.validate_on_submit():
+    #     file = request.files['photo']
+    #     filename = secrets.token_urlsafe(16) + ""
+    #     file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    #     print(url_for('uploads', filename=filename))
+    # else:
+    #     print('error', form.errors) separate uploading logic from page and form to another route
     return render_template('submit.html', title='Submit', form=form)
 
 @app.route('/logout')
