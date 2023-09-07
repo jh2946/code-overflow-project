ac7fe2b709a10bc92fa0b0e44847ea1a0bb87346
jh2946 <junhong2946@gmail.com>
2023-09-07 18:52:56 +0800

    rerouting of form routes

--- app/routes.py
+++ app/routes.py
@@ -46,22 +46,13 @@ start = [0]
 passed = [0]
 pack = [[]]
 num = [0]
-@app.route('/upload', methods=['POST'])
-def upload():
-    file = request.files.getlist("img")
-    for f in file:
-        filename = secure_filename(str(num[0] + 500) + '.jpg')
-        num[0] += 1
-        name = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-        print('save name', name)
-        f.save(name)
-
-    pack[0] = []
-    return render_template('submit.html', img=file, name=name)
 
 import tensorflow
 import csv
 from tensorflow.python.keras.models import load_model
+
+tensorflow.keras.backend.clear_session()
+model_best = load_model('best_model_101class.h5', compile=False)
 # define label meaning
 label = ['apple pie',
          'baby back ribs',
@@ -189,70 +180,41 @@ with open('nutrition101.csv', 'r') as file:
             {'name': 'carbohydrates', 'value': float(row[5])},
             {'name': 'vitamins', 'value': float(row[6])}
         ]
-@app.route('/predict')
-def predict():
-    # Loading the best saved model to make predictions.
+@app.route('/upload', methods=['POST'])
+def upload():
     tensorflow.keras.backend.clear_session()
     model_best = load_model('best_model_101class.h5', compile=False)
-    # model_best = load_model('model_trained_101class.h5', compile=False)
-    print('model successfully loaded!')
-    result = []
-    # pack = []
-    print('total image', num[0])
-    for i in range(start[0], num[0]):
-        pa = dict()
+    file = request.files.getlist("img")
+    for f in file:
+        filename = secure_filename(str(num[0] + 500) + '.jpg')
+        num[0] += 1
+        name = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        print('save name', name)
+        f.save(name)
+    pred_img = name
+    pred_img = image.load_img(pred_img, target_size=(200, 200))
+    pred_img = image.img_to_array(pred_img)
+    pred_img = np.expand_dims(pred_img, axis=0)
+    pred_img = pred_img / 255.
 
-        filename = f'{app.config["UPLOAD_FOLDER"]}/{i + 500}.jpg'
-        print('image filepath', filename)
-        pred_img = filename
-        pred_img = image.load_img(pred_img, target_size=(200, 200))
-        pred_img = image.img_to_array(pred_img)
-        pred_img = np.expand_dims(pred_img, axis=0)
-        pred_img = pred_img / 255.
+    pred = model_best.predict(pred_img)
 
-        pred = model_best.predict(pred_img)
-        print("Pred")
-        print(pred)
-
-        if math.isnan(pred[0][0]) and math.isnan(pred[0][1]) and \
-                math.isnan(pred[0][2]) and math.isnan(pred[0][3]):
-            pred = np.array([0.05, 0.05, 0.05, 0.07, 0.09, 0.19, 0.55, 0.0, 0.0, 0.0, 0.0])
-
-        top = pred.argsort()[0][-3:]
-        label.sort()
-        _true = label[top[2]]
-        # pa['image'] = f'{app.config["UPLOAD_FOLDER"]}/{i + 500}.jpg'
-        pa['image'] = filename
-        x = dict()
-        x[_true] = float("{:.2f}".format(pred[0][top[2]] * 100))
-        x[label[top[1]]] = float("{:.2f}".format(pred[0][top[1]] * 100))
-        x[label[top[0]]] = float("{:.2f}".format(pred[0][top[0]] * 100))
-        pa['result'] = x
-        pa['nutrition'] = nutrition_table[_true]
-        pa['food'] = f'{nu_link}{_true}'
-        pa['idx'] = i - start[0]
-        pa['quantity'] = 100
-
-        pack[0].append(pa)
-        passed[0] += 1
-
-    start[0] = passed[0]
-    print('successfully packed')
-    # compute the average source of calories
-    for p in pack[0]:
-        nutrients[0]['value'] = (nutrients[0]['value'] + p['nutrition'][0]['value'])
-        nutrients[1]['value'] = (nutrients[1]['value'] + p['nutrition'][1]['value'])
-        nutrients[2]['value'] = (nutrients[2]['value'] + p['nutrition'][2]['value'])
-        nutrients[3]['value'] = (nutrients[3]['value'] + p['nutrition'][3]['value'])
-        nutrients[4]['value'] = (nutrients[4]['value'] + p['nutrition'][4]['value'])
-
-    nutrients[0]['value'] = nutrients[0]['value'] / num[0]
-    nutrients[1]['value'] = nutrients[1]['value'] / num[0]
-    nutrients[2]['value'] = nutrients[2]['value'] / num[0]
-    nutrients[3]['value'] = nutrients[3]['value'] / num[0]
-    nutrients[4]['value'] = nutrients[4]['value'] / num[0]
-
-    return render_template('results.html', pack=pack[0], whole_nutrition=nutrients)
+    pa = dict()
+    top = pred.argsort()[0][-3:]
+    label.sort()
+    _true = label[top[2]]
+    # pa['image'] = f'{app.config["UPLOAD_FOLDER"]}/{i + 500}.jpg'
+    pa['image'] = f'/uploads?filename={filename}'
+    x = dict()
+    x[_true] = float("{:.2f}".format(pred[0][top[2]] * 100))
+    x[label[top[1]]] = float("{:.2f}".format(pred[0][top[1]] * 100))
+    x[label[top[0]]] = float("{:.2f}".format(pred[0][top[0]] * 100))
+    pa['result'] = x
+    pa['nutrition'] = nutrition_table[_true]
+    pa['food'] = f'{nu_link}{_true}'
+    pa['idx'] = i - start[0]
+    pa['quantity'] = 100
+    return render_template('results.html', p=pa)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -269,9 +231,9 @@ def login():
             flash('Login Unsuccessful. Please check email and password', 'danger')
     return render_template('login.html', title='Login', form=form)
 
-# @app.route('/uploads')
-# def uploads():
-#     return send_file(os.path.join(app.config['UPLOAD_FOLDER'], request.args['filename']))
+@app.route('/uploads')
+def uploads():
+    return send_file(os.path.join('static\\uploads\\', request.args['filename']))
 
 
 @app.route('/logout')
